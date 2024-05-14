43109799b218127819c1ad457b6d0ace2d55cd30
Bram Mortier <79015250+BramMortier@users.noreply.github.com>
2024-05-14 16:30:58 +0200

    feat: add Campton font and tested font weights

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,7 +1,59 @@
-<script setup></script>
+<script setup>
+import "@/assets/styles/index.scss"
+</script>
 
 <template>
-  <h1>Let's get it!</h1>
+    <div class="typo-test">
+        <p>Campton Black</p>
+        <p>Campton ExtraBold</p>
+        <p>Campton Bold</p>
+        <p>Campton SemiBold</p>
+        <p>Campton Medium</p>
+        <p>Campton Book</p>
+        <p>Campton Light</p>
+        <p>Campton Extra Light</p>
+        <p>Campton Thin</p>
+    </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.typo-test {
+    font-family: "Campton", sans-serif;
+
+    p:nth-child(1) {
+        font-weight: 900;
+    }
+
+    p:nth-child(2) {
+        font-weight: 800;
+    }
+
+    p:nth-child(3) {
+        font-weight: 700;
+    }
+
+    p:nth-child(4) {
+        font-weight: 600;
+    }
+
+    p:nth-child(5) {
+        font-weight: 500;
+    }
+
+    p:nth-child(6) {
+        font-weight: 400;
+    }
+
+    p:nth-child(7) {
+        font-weight: 300;
+    }
+
+    p:nth-child(8) {
+        font-weight: 200;
+    }
+
+    p:nth-child(9) {
+        font-weight: 100;
+    }
+}
+</style>
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -2,13 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: () => import('../views/HomeView.vue')
-    }
-  ]
+  routes: []
 })
 
 export default router
