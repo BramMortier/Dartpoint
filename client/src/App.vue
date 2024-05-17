e7325fa1bedf808ad75b808db6486c607820e1fc
Bram Mortier <79015250+BramMortier@users.noreply.github.com>
2024-05-17 15:49:17 +0200

    chore: remove old dependency code

--- client/src/App.vue
+++ client/src/App.vue
@@ -3,17 +3,11 @@
 // Imports
 // =============================================================================
 import PlaygroundPage from "@/views/PlaygroundPage.vue"
-import { useQuery } from "@tanstack/vue-query"
-import { getUsers } from "@/services/userApi"
 </script>
 
 <template>
     <div class="app">
-        <ul>
-            <li v-for="user in data.body">{{ user.displayName }}</li>
-        </ul>
         <PlaygroundPage />
-        <VueQueryDevtools />
     </div>
 </template>
 
